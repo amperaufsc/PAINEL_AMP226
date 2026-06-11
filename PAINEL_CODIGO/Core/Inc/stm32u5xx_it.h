417c282baf54134fcc767b1c972e8d729cb24f17
maiconampera <maicon.douglas@amperaracing.com>
2026-06-11 20:22:34 -0300

    chekpoint
    checkpoint(compila ainda)

--- PAINEL_CODIGO/Core/Inc/stm32u5xx_it.h
+++ PAINEL_CODIGO/Core/Inc/stm32u5xx_it.h
@@ -56,7 +56,6 @@ void EXTI5_IRQHandler(void);
 void GPDMA1_Channel0_IRQHandler(void);
 void GPDMA1_Channel1_IRQHandler(void);
 void FDCAN1_IT0_IRQHandler(void);
-void FDCAN1_IT1_IRQHandler(void);
 void TIM6_IRQHandler(void);
 void DMA2D_IRQHandler(void);
 void HSPI1_IRQHandler(void);
