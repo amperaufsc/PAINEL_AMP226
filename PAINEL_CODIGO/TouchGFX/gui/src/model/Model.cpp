eea425829a1cd569f648e567634eb004a9ed04da
maiconampera <maicon.douglas@amperaracing.com>
2026-06-12 04:10:03 -0300

    loopback extern funciona
    este comit é uma versao do codgo que funciona perfeitamente o loopbakc extern

--- PAINEL_CODIGO/TouchGFX/gui/src/model/Model.cpp
+++ PAINEL_CODIGO/TouchGFX/gui/src/model/Model.cpp
@@ -11,7 +11,7 @@
 #include <main.h>
 
 #define CAN_ID_RTD  0x141  // ID do botao RTD
-#define CAN_ID_PAG  0x54B  // ID das paginas //0x54B original qualquer outro é teste
+#define CAN_ID_PAG  0x141  // ID das paginas //0x54B original qualquer outro é teste
 #define CAN_ID_SA  0x347  // ID do modo de prova do sistema autonomo
 
 //botao RTD
@@ -228,9 +228,9 @@ void Model::tick()
 		{
 			switch (msg_recebida.id)
 			{
-			case 0x120: { //0x120 //0x141so pra teste
+			case 0x141: { //0x120 //0x141so pra teste
 							falha_inversor = msg_recebida.data[0];
-							readtodrive_led = msg_recebida.data[3];
+							readtodrive_led = msg_recebida.data[0];
 							falha_tms = msg_recebida.data[4];
 							falha_ecu = ((uint16_t)msg_recebida.data[1] << 8) | msg_recebida.data[2];
 							if (readtodrive_led == 3) {  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);
